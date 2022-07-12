9aa5b6196319a98e98003e0e8b614ce606971eba
vinigiu <vinigiu@hotmail.com>
2022-07-12 13:30:59 -0300

    adicionando upload de imagem na criação de produtos

--- controllers/manipularListaProdutosController.js
+++ controllers/manipularListaProdutosController.js
@@ -8,7 +8,7 @@ const manipularListaProdutosController = {
         res.render('criar-produto')
     },
     saveCriar: (req,res) => {
-        const newProduct = {id:null,nome:null,preco:null,valorParcela:null,descricao:null}
+        const newProduct = {id:null,nome:null,preco:null,valorParcela:null,descricao:null,image1:null}
 		const arraylast = listaProdutos.pop() //para pegar o ultimo produto da lista
 		//atribuindo valores do produto
 		newProduct.id = arraylast.id + 1 ;
@@ -16,6 +16,7 @@ const manipularListaProdutosController = {
 		newProduct.preco = req.body.preco;
 		newProduct.valorParcela = req.body.valorParcela;
 		newProduct.descricao = req.body.descricao;
+        newProduct.image1 = req.file.filename;
 		//escrevendo a lista de produtos atualizada na base de dados JSON
 		listaProdutos.push(arraylast);
 		listaProdutos.push(newProduct);
--- routes/manipularListaProdutos.js
+++ routes/manipularListaProdutos.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
-const manipularListaProdutosController = require('../controllers/manipularListaProdutosController')
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const manipularListaProdutosController = require('../controllers/manipularListaProdutosController');
+
+const storage = multer.diskStorage({
+    destination:(req,file,callback) => {
+        let caminho = path.join(__dirname, '../public/images/products');
+        callback(null, caminho)
+    },
+    filename:(req,file,callback) => {
+        let nome = Date.now() + file.originalname;
+        callback(null, nome)
+    }
+});
+
+const upload = multer ({storage:storage});
+
 
 router.get('/criar', manipularListaProdutosController.create);
-router.post('/criar', manipularListaProdutosController.saveCriar);
+router.post('/criar', upload.single('imgNovo'), manipularListaProdutosController.saveCriar);
 
 router.get('/deletar', manipularListaProdutosController.deletar);
 router.post('/deletar', manipularListaProdutosController.saveDeletar);
