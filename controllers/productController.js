462250d60af319144ab5c2ab47e1368e82d23fa6
vinigiu <vinigiu@hotmail.com>
2022-09-02 17:51:58 -0300

    Adicionando calculo de frete com API correios

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,26 @@
 const db = require('../models')
+const correiosController = require('../controllers/correiosController')
+const fs = require('fs')
 
 const productController = {
-    show: async (req,res) => {
+    show: async (req,res,next) => {
         const produtoID = req.params.id;
         const produto = await db.Produto.findByPk(produtoID);
-        res.render('product', {produto:produto})
+
+        res.render('product', {produto:produto, resultado:'undefined'})
     },
+
+    getPreco: async (req,res) => {
+        const produtoID = req.params.id;
+        const produto = await db.Produto.findByPk(produtoID);
+
+        correiosController.calculaPreco(req,res)
+
+        const resultado = await JSON.parse(fs.readFileSync(`${__dirname}\\assetCorreio\\correios.json`))
+        const resultadoTratado = resultado.Servicos.cServico[0].Valor[0]
+
+        res.render('product', {produto:produto, resultado:resultadoTratado})
+    }
 
 }
 
