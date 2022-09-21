2c26d1f80289032c6e924a84a9f60b30e194f082
xprangex <arthur.prange@gmail.com>
2022-09-21 19:58:14 -0300

    retificações de rota

--- config/config.js
+++ config/config.js
@@ -4,20 +4,21 @@ module.exports = {
     password: null,
     database: "ze_bone",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
+    port: 3307,
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
     host: "127.0.0.1",
-    dialect: "mysql"
-  }
-}
+    dialect: "mysql",
+  },
+};
