6eeb82ff7a596dd8e8777e3fa773dfba919147c6
vinigiu <vinigiu@hotmail.com>
2022-09-24 09:38:36 -0300

    Inserindo página de erro para não logado

--- middlewares/jwtVerify.js
+++ middlewares/jwtVerify.js
@@ -4,7 +4,7 @@ const SECRET = require('../config/secret')
 function jwtVerify(req,res,next){
     const token = req.session.token
     jwt.verify(token, SECRET, (err,decoded) => {
-        if(err) return res.status(401).send("<script>alert('Você precisa estar logado'); window.location.href = '/'; </script>");;
+        if(err) return res.status(401).render('notLogged');
         req.user = decoded.user;
         next()
     })
