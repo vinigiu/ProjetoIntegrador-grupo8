d0cc1ec1a983cdc9f6ad37c97575cc8accfe8cb5
Daniel Barbieri <danielbarbierianalista@gmail.com>
2022-09-26 16:58:09 -0300

    links footer

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
   development: {
     username: "root",
-    password: null,
+    password: "Daniel2609",
     database: "ze_bone",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   test: {
     username: "root",
-    password: null,
+    password: "Daniel2609",
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   production: {
     username: "root",
-    password: null,
+    password: "Daniel2609",
     database: "database_production",
     host: "127.0.0.1",
     dialect: "mysql"
