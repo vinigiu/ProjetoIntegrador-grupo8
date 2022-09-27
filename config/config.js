fa9b5f1318f0da1f3e654ac825e5cf1ab1386549
vinigiu <103456346+vinigiu@users.noreply.github.com>
2022-09-27 09:09:45 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
   development: {
     username: "root",
-    password: "",
+    password: null,
     database: "ze_bone",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   test: {
     username: "root",
-    password: "",
+    password: null,
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   production: {
     username: "root",
-    password: "",
+    password: null,
     database: "database_production",
     host: "127.0.0.1",
     dialect: "mysql"
